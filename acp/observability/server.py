56d7981750df20abdf2f9da732887ec11da01779
aussiearef <arefkr@gmail.com>
2025-07-21 10:57:42 +1000

    Added Logs Example

--- acp/observability/server.py
+++ acp/observability/server.py
@@ -5,24 +5,30 @@ from beeai_framework.backend.chat import ChatModel # only needed if Agent is usi
 from beeai_framework.agents.react import ReActAgent
 from beeai_framework.memory.token_memory import TokenMemory
 
-from opentelemetry import metrics
+from opentelemetry import metrics 
+from opentelemetry._logs import get_logger , LogRecord
 
 
 server = Server()
-meter = metrics.get_meter(name="otel.acp.example.metric")
 
+meter = metrics.get_meter(name="otel.acp.example.metric")
 request_counter = meter.create_counter(
     "otel.meaningagent.run_count",
     description="Total requests processed",
     unit="1"
 )
 
+logger = get_logger("ACP Server")
+logger.emit(LogRecord(
+    body="Starting ACP Server"
+))
 
 @server.agent("MeaningAgent")
 async def meaning(words: Message) -> str:
     
     request_counter.add(1, {"agent": "MeaningAgent"})
 
+    logger.emit("Calling OPEN AI")
     llm= ChatModel.from_name("openai:gpt-4o")  
 
     memory = TokenMemory(llm)

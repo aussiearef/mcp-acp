7f2250d0ae0f859d8d8311f0b2f01c192527fd49
Aref Karimi <arefkr@gmail.com>
2026-01-15 08:19:06 +1100

    Added a2a/basic

---
+++ a2a/basic/server.py
@@ -0,0 +1,42 @@
+from a2a.server.apps import A2AFastAPIApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.types import AgentCard, AgentCapabilities, AgentSkill
+
+from executor import (
+    HelloWorldAgentExecutor
+)
+
+if __name__ =="__main__":
+    agent_skill = AgentSkill(
+        id="say_hello_skill",
+        name="Hello Skill",
+        description="I can say hello",
+        tags=["Hello", "World"]
+    )
+
+    agent_capabiliy = AgentCapabilities(streaming=False)
+
+    agent_card = AgentCard(
+        name="Basic Server Agent (HelloWorld)",
+        description="",
+        version="1.0",
+        url="http://localhost:9999",
+        default_input_modes=["text"],
+        default_output_modes=["text"],
+        skills=[agent_skill],
+        capabilities=agent_capabiliy
+    )
+
+    request_handler = DefaultRequestHandler(
+        agent_executor= HelloWorldAgentExecutor(),
+        task_store=InMemoryTaskStore()
+    )
+
+    server = A2AFastAPIApplication(
+        http_handler= request_handler,
+        agent_card=agent_card
+    )
+
+    import uvicorn
+    uvicorn.run(server.build() , host="0.0.0.0" , port=9999)
